c0a7a9910b4db6e840ba2c8134990add5f49aab9
mrrobot <kooldude5347@ahoo.com>
2019-03-24 21:37:17 -0400

    adding files

---
+++ instaUnfollow/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace instaUnfollow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(80, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(19, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(22, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(19, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(80, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(22, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Unfollow";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(19, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Unfollow Count:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(107, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "0";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Black;
+            this.textBox3.ForeColor = System.Drawing.Color.White;
+            this.textBox3.Location = new System.Drawing.Point(270, 19);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(217, 206);
+            this.textBox3.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(22, 61);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(18, 19);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login To Instagram";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.ForeColor = System.Drawing.Color.White;
+            this.groupBox2.Location = new System.Drawing.Point(18, 125);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Controls";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.groupBox1);
+            this.groupBox3.Controls.Add(this.groupBox2);
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.ForeColor = System.Drawing.Color.Black;
+            this.groupBox3.Location = new System.Drawing.Point(12, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(522, 247);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(551, 276);
+            this.Controls.Add(this.groupBox3);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Un-Follower";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ instaUnfollow/Form1.cs
@@ -0,0 +1,436 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace instaUnfollow
+{
+    //Create by chris campone
+    //This tool is used to unfollow users that you follow on Instagram
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ChromeDriver driver;
+        string LoginBtnClass = "L3NKy";
+        string profileIconXPath = "//*[@id=\"react-root\"]/section/nav/div[2]/div/div/div[3]/div/div[3]/a/span";
+        string followModel = "isgrP";
+        string modelUsernameClass = "_0imsa";
+        string followingBtnClass = "BY3EC";
+
+        bool forceStop = false;
+        int amountUnFollowed = 0;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            createDriver(false);
+
+        }
+
+        //creates the driver (chrome window)
+        private void createDriver(bool headless)
+        {
+            try
+            {
+                //delete driver if one is open
+                if (driver != null && driver.WindowHandles.Count > 0)
+                {
+                    driver.Quit();
+                }
+                ChromeDriverService driverService = ChromeDriverService.CreateDefaultService();
+                driverService.HideCommandPromptWindow = true;
+                ChromeOptions options = new ChromeOptions();
+                if (headless)
+                {
+                    //hide the broswer from sight
+                    options.AddArgument("--headless");
+                }
+                //  options.EnableMobileEmulation("iPhone X");
+                driver = new ChromeDriver(driverService, options);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        //quit driver when closing form
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            driver.Quit();
+        }
+
+        //search webpages and wait for element to appear so load is completed
+        async public Task search(string url, string xPathElementToWaitFor)
+        {
+            int trys = 0;
+            driver.Navigate().GoToUrl(url);
+            if (xPathElementToWaitFor.Length > 0)
+            {
+
+                await waitForElement(xPathElementToWaitFor);
+            }
+            else
+            {
+                if (trys >= 10)
+                {
+                    return;
+                }
+                trys++;
+                await Task.Delay(1000);
+            }
+        }
+
+        //method to wait for elements to appear
+        async Task waitForElement(string xPath)
+        {
+            try
+            {
+                await Task.Delay(500);
+                int trys = 0;
+                int count = 0;
+                do
+                {
+                    if (xPath.Contains("/"))
+                    {
+                        var links = driver.FindElementsByXPath(xPath);
+                        count = links.Count;
+                    }
+                    else
+                    {
+                        var links = driver.FindElementsByClassName(xPath);
+                        count = links.Count;
+                    }
+
+                    trys++;
+                    await Task.Delay(1000);
+                } while (count == 0 && trys < 10);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        //checks for existing element
+        private bool checkIfElementExists(string xPath)
+        {
+            try
+            {
+                if (xPath.Contains("/"))
+                {
+                    var links = driver.FindElementsByXPath(xPath);
+                    if (links.Count > 0)
+                    {
+                        return true;
+
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    var links = driver.FindElementsByClassName(xPath);
+                    if (links.Count > 0)
+                    {
+                        return true;
+
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        //add text to log
+        private void cmd(string x)
+        {
+            string[] lines = textBox3.Text.Split(new Char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            int count = lines.Length;
+
+            if (count >= 17)
+            {
+                textBox3.Text = "";
+                textBox3.Text += x + Environment.NewLine;
+            }
+            else
+            {
+                textBox3.Text += x + Environment.NewLine;
+            }
+        }
+
+        //login method
+        async Task loginInstagram()
+        {
+
+            try
+            {
+                if (textBox1.Text.Contains("@"))
+                {
+                    cmd("Please use your username to log in");
+                    return;
+                }
+                button1.Enabled = false;
+                await search("https://www.instagram.com/accounts/login/", LoginBtnClass);
+                if (driver.Url.Equals("https://www.instagram.com/"))
+                {
+                    cmd("You are already logged in");
+                    return;
+                }
+                if (checkIfElementExists("KPZNL"))
+                {
+                    driver.FindElementByClassName("KPZNL").Click();
+                    await Task.Delay(250);
+                }
+                cmd("Logging into instagram");
+                driver.FindElement(By.Name("username")).SendKeys(textBox1.Text);
+                driver.FindElement(By.Name("password")).SendKeys(textBox2.Text);
+                await Task.Delay(500);
+                driver.FindElement(By.ClassName(LoginBtnClass)).Click();
+                await waitForElement(profileIconXPath);
+                if (checkIfElementExists("_3m3RQ"))
+                {
+                    driver.FindElementByClassName("_7XMpj").Click();
+                    await Task.Delay(350);
+                }
+                else if (checkIfElementExists("O4QwN"))
+                {
+                    if (driver.FindElementByClassName("O4QwN").Text.Contains("Suspicious Login Attempt"))
+                    {
+                        button1.Enabled = true;
+                        textBox2.Text = "";
+                        cmd("Suspicious Login Attempt, Please Fix");
+                        return;
+                    }
+                }
+                if (checkIfElementExists(profileIconXPath))
+                {
+                    //check for pop up
+                    if (checkIfElementExists("/html/body/div[2]/div/div/div"))
+                    {
+                        driver.FindElementByClassName("HoLwm").Click();
+                    }
+                    cmd("Logged In");
+                    button1.Enabled = false;
+                    button1.Text = "Logged In";
+                }
+                else
+                {
+                    button1.Enabled = true;
+                    textBox2.Text = "";
+                    if (driver.Url.ToLower().Contains("challenge"))
+                    {
+                        cmd("Your account is locked with a challenge. Please fix this before using the bot");
+                    }
+                    else
+                    {
+                        cmd("Wrong credentials?");
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+               
+
+                //MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        //login btn
+        async private void button1_Click(object sender, EventArgs e)
+        {
+            await loginInstagram();
+        }
+
+        //unfollow users
+        async private void button2_Click(object sender, EventArgs e)
+        {
+            await unfollowUsernames();
+        }
+
+        //unfollows users not following you
+        async private Task unfollowUsernames()
+        {
+            try
+            {
+                
+                do
+                {
+                    ArrayList usersToUnfollow = new ArrayList();
+                    await search("https://www.instagram.com/" + textBox1.Text + "/", profileIconXPath);
+                    var links = driver.FindElementsByClassName("-nal3"); //gets the links followers,following and pictureCount
+                    cmd("Gathering Users to unfollow");
+                    foreach (IWebElement item in links)
+                    {
+                        //click on following
+                        if (item.Text.Contains("following"))
+                        {
+                            item.Click();
+                            await Task.Delay(1500);
+                            break;
+                        }
+                    }
+                    await waitForElement(followModel);
+                    driver.FindElement(By.ClassName(followModel)).Click();//clicks on model to page down
+                    await Task.Delay(1000);
+                    for (int i = 0; i < 10; i++)
+                    {
+                        driver.FindElement(By.ClassName(followModel)).Click();//clicks on model to page down
+                        new Actions(driver).SendKeys(OpenQA.Selenium.Keys.PageDown).Perform();
+                        await Task.Delay(50);
+                        if (i < 3)
+                        {
+                            await Task.Delay(1000);
+                        }
+                    }
+                    foreach (var item in driver.FindElements(By.ClassName(modelUsernameClass)))//class to get username of user
+                    {
+                        if (item.Text.Contains("Verified"))
+                        {
+                            usersToUnfollow.Add(item.Text.Substring(0, item.Text.IndexOf("Verified")));
+                        }
+                        else
+                        {
+                            usersToUnfollow.Add(item.Text);
+                        }
+
+                    }
+                    string user = "";
+                    if (usersToUnfollow.Count == 0)
+                    {
+                        cmd("You have no users to unfollow");
+                        return;
+                    }
+                    cmd("Gathered: " + usersToUnfollow.Count);
+                    foreach (string item in usersToUnfollow)
+                    {
+                        if(forceStop){
+                            forceStop = false;
+                            return;
+                        }
+                        //unfollow each user
+                        user = item;
+                        await search("https://www.instagram.com/" + user + "/", followingBtnClass);
+                        if ((driver.PageSource.Contains("\"follows_viewer\":false")) && !driver.PageSource.Contains("Sorry, this page isn't available"))
+                        {
+                            var btns = driver.FindElementsByClassName(followingBtnClass);
+                            foreach (IWebElement btn in btns)
+                            {
+                                if (driver.PageSource.Contains("\"followed_by_viewer\":true"))
+                                {
+                                    btn.Click();
+                                    await Task.Delay(1000);
+                                    if (checkIfElementExists("pbNvD"))//pop up unfollow
+                                    {
+                                        var unfollowBtns = driver.FindElementsByClassName("aOOlW");
+                                        foreach (IWebElement itemBtn in unfollowBtns)
+                                        {
+                                            if (itemBtn.Text.Contains("Unfollow"))
+                                            {
+                                                itemBtn.Click();
+                                                break;
+                                            }
+                                        }
+                                        for (int i = 0; i < 10; i++)
+                                        {
+                                            if (!driver.FindElementByClassName(followingBtnClass).Text.Equals("Follow"))
+                                            {
+                                                await Task.Delay(300);
+                                            }
+                                            else
+                                            {
+                                                break;
+                                            }
+
+                                        }
+                                        await search("https://www.instagram.com/" + user + "/", followingBtnClass);
+                                        if (driver.PageSource.Contains("\"followed_by_viewer\":false"))
+                                        {
+                                            amountUnFollowed++;
+                                            cmd("Un-Followed: " + user);
+                                            label4.Text = amountUnFollowed.ToString();
+                                        }
+                                        else if (driver.PageSource.Contains("\"followed_by_viewer\":true"))
+                                        {
+                                            cmd("Temp Un-Follow blocked");
+                                            for (int i = 180; i >0; i--)
+                                            {
+                                                cmd("Spam Wait: "+i.ToString());
+                                                await Task.Delay(1000);
+                                            }
+                                        }
+                                    }
+                                    else
+                                    {
+                                        await search("https://www.instagram.com/" + user + "/", "_5f5mN");
+                                        if (driver.PageSource.Contains("\"followed_by_viewer\":false"))
+                                        {
+                                            amountUnFollowed++;
+                                            cmd("Un-Followed: " + user);
+                                            label4.Text = amountUnFollowed.ToString();
+                                        }
+                                        else if (driver.PageSource.Contains("\"followed_by_viewer\":true"))
+                                        {
+                                            cmd("Temp Un-Follow blocked");
+                                            for (int i = 180; i > 0; i--)
+                                            {
+                                                cmd("Spam Wait: " + i.ToString());
+                                                await Task.Delay(1000);
+                                            }
+                                        }
+                                    }
+
+                                }
+                                
+                            }
+                        }
+                        else
+                        {
+                            cmd("User is following you: " + user);
+                        }
+                    }//done unfollowing group 
+
+                } while (!forceStop);
+                forceStop = false;
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Unfollowing: "+ex.ToString());
+                // screenshot("error");
+            }
+        }
+
+        //stop btn
+        private void button3_Click(object sender, EventArgs e)
+        {
+            forceStop = true;
+        } 
+
+
+    }
+}
